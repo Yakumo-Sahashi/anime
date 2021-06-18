cce7d055aa4c49a58aa648e850822e1fb04b99ea
Yakumo-Sahashi <diego.bollas@hotmail.com>
2021-06-18 09:49:43 -0500

    direccion como constante

--- app/config.php
+++ app/config.php
@@ -2,5 +2,16 @@
 	define('NOMBRE_SERVIDOR', 'localhost');
     define("SERVIDOR", "http://localhost/animeHikari/");
     define("SQL", SERVIDOR."sql/");
-    session_start();
+    define("direccion", array(
+        'home' => 'view/home',
+        'login' => 'view/login/login',
+        'registro' => 'view/login/registro',
+        'recuperar' => 'view/login/recuperar',
+        'panel' => 'view/admin/panel',
+        'usuario' => 'view/admin/usuarios',
+        'anime' => 'view/admin/anime',
+        'cuenta' => 'view/usuario/usuario',
+        'lista' => 'view/anime/lista_anime',
+        'ver' => 'view/anime/anime',
+    ));
  ?>
